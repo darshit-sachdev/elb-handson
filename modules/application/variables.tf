ce8589e497d6027ef88729170d4b86b80b7a6c14
darshit-sachdev <89072790+darshit-sachdev@users.noreply.github.com>
2021-08-23 17:31:02 +0530

    Update variables.tf

--- modules/application/variables.tf
+++ modules/application/variables.tf
@@ -1,10 +1,11 @@
 variable "AWS_REGION" {
-  default = "us-east-1"
+  default = "us-east-2"
 }
 variable "AMIS" {
   type = map(string)
   default = {
     us-east-1 = "ami-13be557e"
+    us-east-2 = "ami-062a21099569f9137"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-844e0bf7"
   }
