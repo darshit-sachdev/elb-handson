e26f6a939a7bd163db71fdc4f2d67c522688647d
darshit-sachdev <89072790+darshit-sachdev@users.noreply.github.com>
2021-08-23 17:59:48 +0530

    Update variables.tf

--- modules/application/variables.tf
+++ modules/application/variables.tf
@@ -5,7 +5,7 @@ variable "AMIS" {
   type = map(string)
   default = {
     us-east-1 = "ami-13be557e"
-    us-east-2 = "ami-062a21099569f9137"
+    us-east-2 = "ami-0dfa0bf531cde9048"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-844e0bf7"
   }
